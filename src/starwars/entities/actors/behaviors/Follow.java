e311ceba5309e2c161e1c0557fb767bb33c26ad1
alexminagall <aminagall3@gmail.com>
2018-05-13 19:09:15 +0930

    if it is in the same location as its owner, it will stay there.

--- src/starwars/entities/actors/behaviors/Follow.java
+++ src/starwars/entities/actors/behaviors/Follow.java
@@ -4,22 +4,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
+import edu.monash.fit2099.gridworld.Grid;
+import edu.monash.fit2099.gridworld.Grid.CompassBearing;
 import edu.monash.fit2099.simulator.matter.EntityManager;
 import edu.monash.fit2099.simulator.space.Direction;
 import starwars.SWAction;
 import starwars.SWActor;
 import starwars.SWEntityInterface;
 import starwars.SWLocation;
+import starwars.SWWorld;
+import starwars.actions.Move;
 import starwars.entities.actors.Droid;
 
 public class Follow {
 
 	
-	public Follow(SWActor owner, Droid droid) {
+	public Follow(SWActor owner, Droid droid, SWWorld world) {
+		SWLocation locationOfDroid = SWAction.getEntitymanager().whereIs((SWEntityInterface) droid);
+		SWLocation locationOfOwner = SWAction.getEntitymanager().whereIs((SWEntityInterface) owner);
 		
+		
+		
+		if (locationOfOwner == locationOfDroid){
+			return;
+		}
 	}
-
-
-	
-	
 }
+		
+
+
+
+	
+	
+
