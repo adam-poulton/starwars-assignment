0edd7134f0528052d976a654acf14c6611dae188
pada6 <pada6@student.monash.edu>
2018-05-16 16:09:29 +1000

    reservoir takeDamage method override

--- src/starwars/entities/Reservoir.java
+++ src/starwars/entities/Reservoir.java
@@ -38,6 +38,10 @@ public class Reservoir extends SWEntity {
 		this.setLongDescription("a water reservoir.");
 		this.setShortDescription("a water reservoir, full of cool, clear, refreshing water");
 		this.setSymbol("W");
+
+		//reservoir requirement - starting hitpoints
+        //adamp 16/05/18
+		this.setHitpoints(40);
 	}
 
 	@Override 
@@ -47,5 +51,41 @@ public class Reservoir extends SWEntity {
 	
 	public String getLongDescription() {
 		return longDescription;
+	}
+
+    /**
+     * Method calls the super method and then will,
+     * <ul>
+     *     <li>Check the remaining <code>hitpoints</code></li>
+     *     <li>If <code>0 < hitpoints < 20</code>:</li>
+     *     <ul>
+     *         <li>Set the short description of this <code>Reservoir</code> to "a damaged water reservoir"</li>
+     * 	       <li>Set the long description of this <code>Reservoir</code> to "a damaged water reservoir, leaking slowly"</li>
+     * 	       <li>Set the symbol of this <code>Reservoir</code> to "V"</li>
+     * 	   </ul>
+     * 	   <li>If <code>hitpoints <= 0</code>:</li>
+     *     <ul>
+     *         <li>Set the short description of this <code>Reservoir</code> to "the wreckage of a water reservoir"</li>
+     * 	       <li>Set the long description of this <code>Reservoir</code> to "the wreckage of a water reservoir, surrounded by slightly damp soil"</li>
+     * 	       <li>Set the symbol of this <code>Reservoir</code> to "X"</li>
+     * 	   </ul>
+     * </ul>
+     * @param damage amount of damage this <code>Reservoir</code> will take
+     *
+     * @author adamp 16/05/18
+     */
+	@Override
+	public void takeDamage(int damage){
+		super.takeDamage(damage);
+		int hp = this.getHitpoints();
+		if (hp > 0 && hp < 20) {
+            this.setShortDescription("a damaged water reservoir");
+            this.setLongDescription("a damaged water reservoir, leaking slowly");
+            this.setSymbol("V");
+        } else if (hp <= 0) {
+		    this.setShortDescription("the wreckage of a water reservoir");
+		    this.setLongDescription("the wreckage of a water reservoir, surrounded by slightly damp soil");
+		    this.setSymbol("X");
+        }
 	}
 }
